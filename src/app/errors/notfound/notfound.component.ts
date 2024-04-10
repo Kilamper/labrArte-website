f2c5a40d3d8f811803d6233117d1a82331d9d7cc
Kilamper <kilamper@gmail.com>
2024-04-10 13:41:01 +0100

    Redirect from 404 page to home after 5 seconds

---
+++ src/app/errors/notfound/notfound.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notfound.component.html',
+})
+export class NotfoundComponent implements OnInit {
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 5000);
+  }
+}
