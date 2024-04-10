30019c99ec20a3cb68096c2c9333f3b063b40bb7
Kilamper <kilamper@gmail.com>
2024-04-10 15:09:40 +0100

    Change html template for inline template on typescript file

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -2,7 +2,10 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html'
+  template: '<a href="../pages/category.html?id={{categoryId}}" class="text-center">\n' +
+    '  <img class="rounded-xl w-80 border border-gray-400" [src]="categoryImage" alt="categoryName">\n' +
+    '  <p class="text-xl font-semibold">{{ categoryName }}</p>\n' +
+    '</a>'
 })
 export class CategoryComponent {
   @Input() categoryId: number = 0;
