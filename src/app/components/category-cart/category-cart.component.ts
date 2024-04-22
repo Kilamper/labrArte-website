06e5fc8bc8027e3fc18d45057d96c7e9d7dbb73f
Kilian Armas Pérez <kilamper@gmail.com>
2024-04-22 11:49:51 +0100

    Create scroll service and add it to category-cart, product-cart and footer redirections

--- src/app/components/category-cart/category-cart.component.ts
+++ src/app/components/category-cart/category-cart.component.ts
@@ -1,10 +1,11 @@
 // src/app/components/category-cart/category-cart.component.ts
 import {Component, Input} from '@angular/core';
+import {ScrollService} from '../../services/scroll/scroll.service';
 
 @Component({
   selector: 'app-category-cart',
   template: `
-    <a [routerLink]="['/category', categoryId]" class="text-center">
+    <a (click)="scrollService.scrollToTop()" [routerLink]="['/category', categoryId]" class="text-center">
       <img class="rounded-xl w-80 border border-gray-400" [src]="categoryImage" alt="categoryName">
       <p class="text-xl font-semibold">{{ categoryName }}</p>
     </a>
@@ -14,4 +15,7 @@ export class CategoryCartComponent {
   @Input() categoryId: number = 0;
   @Input() categoryName: string = 'Category';
   @Input() categoryImage: string = 'https://th.bing.com/th/id/R.7493e5654057ed64161c7579703d56b4?rik=zhz13r4b96lqZA&pid=ImgRaw&r=0';
+
+  constructor(public scrollService: ScrollService) {
+  }
 }
