de3de01289a979dd4e103022e61274f85deabab5
Kilamper <kilamper@gmail.com>
2024-04-25 20:21:22 +0100

    Fix responsive design of Footer and Header, and extract user button from mobile dropdown menu

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -3,9 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-logo',
   template: '<a href="" (click)="scrollToTop()" class="flex items-center justify-start no-underline">\n' +
-    '  <img alt="image" ngSrc="../../../assets/logo.svg" class="w-12 h-12 lg:w-16 lg:h-16 logo"\n' +
+    '  <img alt="image" ngSrc="../../../assets/logo.svg" class="w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16 logo"\n' +
     '       height="80" width="80">\n' +
-    '  <h1 class="pl-2 text-4xl font-bold lg:text-5xl logo-name">labrArte®</h1>\n' +
+    '  <h1 class="pl-2 text-3xl sm:text-4xl lg:text-5xl font-bold logo-name">labrArte®</h1>\n' +
     '</a>\n',
 })
 export class LogoComponent {
