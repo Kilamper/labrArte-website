79c31b7bd9650dd25d0a057cc76ba836896d05ea
Kilian Armas Pérez <kilamper@gmail.com>
2024-04-29 13:56:23 +0100

    Add product interface to cart item component

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,9 +1,24 @@
-import {Component} from '@angular/core';
+import {Input, Component} from '@angular/core';
+
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-cart-item',
   template: ''
 })
-export class CartItemComponent {
 
+export class CartItemComponent {
+  @Input() product: Product = {
+    id: 0,
+    name: 'Product',
+    image: 'https://th.bing.com/th/id/R.7493e5654057ed64161c7579703d56b4?rik=zhz13r4b96lqZA&pid=ImgRaw&r=0',
+    price: 0,
+    description: 'Description'
+  }
 }
