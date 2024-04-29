b8d4d9eefbd5941ddc7cb99b4f2ab481bcf8a1da
Kilian Armas Pérez <kilamper@gmail.com>
2024-04-29 12:28:52 +0100

    Change button action and add redirection to email in "Contactar" button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,15 +3,23 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-button',
   template: `
-    <a [routerLink]="[url]"
-       class="block py-2 px-3 text-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-navy lg:p-0 hover:text-black menu-links">
+    <a (click)="handleClick()"
+       class="block py-2 px-3 text-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-navy lg:p-0 hover:text-black menu-links cursor-pointer">
       <i class="{{icon}}"></i>
       <span class="pl-2.5">{{ content }}</span>
     </a>
   `,
 })
 export class ButtonComponent {
-  @Input() url: string = '/'
+  @Input() action: string = ''
   @Input() content: string = ''
   @Input() icon: string = ''
+
+  handleClick() {
+    if (this.action.startsWith('/')) {
+      window.location.href = this.action
+    } else if (this.action.startsWith('mailto:')) {
+      window.open(this.action)
+    }
+  }
 }
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -8,13 +8,18 @@ import {ScrollService} from '../../services/scroll/scroll.service';
       <img class="rounded-xl w-80 border-b border-gray-400" [src]="productImage" [alt]="productName">
       <p class="text-xl font-semibold">{{ productName }}</p>
       <p class="text-xl font-semibold">{{ productPrice }} €</p>
+      <div class="flex justify-between px-4 py-2 border-t border-gray-400">
+        <app-button [content]="'Añadir al carrito'" [icon]="'fa-solid fa-cart-plus'"></app-button>
+        <app-button [action]="'/product/' + productId" [content]="'Detalles'"
+                    [icon]="'fa-regular fa-file-lines'"></app-button>
+      </div>
     </a>`
 })
 export class ProductItemComponent {
   @Output() @Input() productId: number = 0;
   @Input() productName: string = 'Product';
   @Input() productImage: string = 'https://th.bing.com/th/id/R.7493e5654057ed64161c7579703d56b4?rik=zhz13r4b96lqZA&pid=ImgRaw&r=0';
-  @Input() productPrice: string = '0€';
+  @Input() productPrice: number = 0;
   @Input() productDescription: string = 'Description';
 
   constructor(public scrollService: ScrollService) {
