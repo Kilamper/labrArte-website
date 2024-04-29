6e35cd91f23fec864c4b8000c34449780a78176a
Kilian Armas Pérez <kilamper@gmail.com>
2024-04-29 13:27:44 +0100

    Change product-item input parameters to an interface

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,27 +1,38 @@
 import {Component, Input, Output} from '@angular/core';
 import {ScrollService} from '../../services/scroll/scroll.service';
 
+interface Product {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+    description: string;
+}
+
 @Component({
-  selector: 'app-product-item',
-  template: `
-    <a (click)="scrollService.scrollToTop()" [routerLink]="['/product', productId]" class="text-center">
-      <img class="rounded-xl w-80 border-b border-gray-400" [src]="productImage" [alt]="productName">
-      <p class="text-xl font-semibold">{{ productName }}</p>
-      <p class="text-xl font-semibold">{{ productPrice }} €</p>
-      <div class="flex justify-between px-4 py-2 border-t border-gray-400">
-        <app-button [content]="'Añadir al carrito'" [icon]="'fa-solid fa-cart-plus'"></app-button>
-        <app-button [action]="'/product/' + productId" [content]="'Detalles'"
-                    [icon]="'fa-regular fa-file-lines'"></app-button>
-      </div>
-    </a>`
+    selector: 'app-product-item',
+    template: `
+        <a (click)="scrollService.scrollToTop()" [routerLink]="['/product', product.id]" class="text-center">
+            <img class="rounded-xl w-80 border-b border-gray-400" [src]="product.image" [alt]="product.name">
+            <p class="text-xl font-semibold">{{ product.name }}</p>
+            <p class="text-xl font-semibold">{{ product.price }} €</p>
+            <div class="flex justify-between px-4 py-2 border-t border-gray-400">
+                <app-button [content]="'Añadir al carrito'" [icon]="'fa-solid fa-cart-plus'"
+                            [product]="product"></app-button>
+                <app-button [action]="'/product/' + product.id" [content]="'Detalles'"
+                            [icon]="'fa-regular fa-file-lines'"></app-button>
+            </div>
+        </a>`
 })
 export class ProductItemComponent {
-  @Output() @Input() productId: number = 0;
-  @Input() productName: string = 'Product';
-  @Input() productImage: string = 'https://th.bing.com/th/id/R.7493e5654057ed64161c7579703d56b4?rik=zhz13r4b96lqZA&pid=ImgRaw&r=0';
-  @Input() productPrice: number = 0;
-  @Input() productDescription: string = 'Description';
+    @Input() product: Product = {
+        id: 0,
+        name: 'Product',
+        image: 'https://th.bing.com/th/id/R.7493e5654057ed64161c7579703d56b4?rik=zhz13r4b96lqZA&pid=ImgRaw&r=0',
+        price: 0,
+        description: 'Description'
+    }
 
-  constructor(public scrollService: ScrollService) {
-  }
+    constructor(public scrollService: ScrollService) {
+    }
 }
