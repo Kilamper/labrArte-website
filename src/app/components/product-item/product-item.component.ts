91d468eb66dac7714645475077c06ae0521f55ba
Kilian Armas Pérez <kilamper@gmail.com>
2024-04-29 13:34:25 +0100

    Quit unused import

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ScrollService} from '../../services/scroll/scroll.service';
 
 interface Product {
