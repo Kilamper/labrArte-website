13046f24d5c85d15e3f01cf58373a4bc21ae3fc8
Kilamper <kilamper@gmail.com>
2024-05-04 14:12:45 +0100

    Add userService and use getUID function to get cart products

--- src/app/templates/shopping-cart/shopping-cart.component.ts
+++ src/app/templates/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CartService} from "../../services/cart/cart.service";
+import {UserService} from "../../services/user/user.service";
 
 @Component({
   selector: 'app-shopping-cart',
@@ -8,12 +9,15 @@ import {CartService} from "../../services/cart/cart.service";
 export class ShoppingCartComponent implements OnInit {
   displayData: any[] = [];
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private userService: UserService) {
   }
 
   ngOnInit(): void {
-    this.cartService.getCart('iPlCcPOE5uYvBZKwnRLzCqQ2d0n2').subscribe((data: any) => {
-      this.displayData = data;
-    });
+    const uid = this.userService.getUID();
+    if (uid) {
+      this.cartService.getCart(uid).subscribe((data: any) => {
+        this.displayData = data;
+      });
+    }
   }
 }
