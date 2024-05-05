396cd07826a9741d3751f50516e6d51ccc3edc7b
Kilamper <kilamper@gmail.com>
2024-05-05 13:14:02 +0100

    Show user icon when user is not logged and show user's personal image when is logged

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -1,18 +1,27 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user/user.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
+  userUID: string | undefined;
+  userImage: string | undefined;
 
-  constructor() {}
-
-  ngOnInit(): void {
-      document.getElementById('burger-menu')?.addEventListener('click', function(): void {
-        const navbarDropdown = document.getElementById('navbar-dropdown');
-        navbarDropdown?.classList.toggle('hidden');
+  constructor(private userService: UserService) {
+    this.userUID = this.userService.getUID();
+    if (this.userUID) {
+      this.userService.getUserData().subscribe((userData: any) => {
+        this.userImage = userData.profilePicture;
       });
+    }
   }
 
+  ngOnInit(): void {
+    document.getElementById('burger-menu')?.addEventListener('click', function (): void {
+      const navbarDropdown = document.getElementById('navbar-dropdown');
+      navbarDropdown?.classList.toggle('hidden');
+    });
+  }
 }
