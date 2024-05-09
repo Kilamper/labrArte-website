0d85efa5c450bba4bb24cb800d9f44a48cd1b3a2
alvarotrancho <alvaro.virgo.99@gmail.com>
2024-05-09 08:37:53 +0100

    removed css in user profile

--- src/app/templates/user-profile/user-profile.component.ts
+++ src/app/templates/user-profile/user-profile.component.ts
@@ -7,7 +7,6 @@ import {User} from "../../interfaces/user.interface";
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit{
 
